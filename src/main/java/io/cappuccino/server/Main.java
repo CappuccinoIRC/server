63641360aa227e013359d90d4209deb805784aa5
Ronald Bunk <me@azoraqua.com>
2021-03-02 07:39:17 +0100

    Updated.
    Signed-off-by: Ronald Bunk <me@azoraqua.com>

--- src/main/java/io/cappuccino/server/Main.java
+++ src/main/java/io/cappuccino/server/Main.java
@@ -1,10 +1,41 @@
 package io.cappuccino.server;
 
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+
 public final class Main {
 
     private Main() { }
 
     public static void main(String[] args) {
-        // TODO: Implement.
+        final WebSocketServer server = new WebSocketServer(new InetSocketAddress(8000)) {
+            @Override
+            public void onOpen(WebSocket conn, ClientHandshake handshake) {
+                System.out.println("Got a connection: " + conn.getRemoteSocketAddress());
+            }
+
+            @Override
+            public void onClose(WebSocket conn, int code, String reason, boolean remote) { }
+
+            @Override
+            public void onMessage(WebSocket conn, String message) {
+                System.out.println("Got a message from " + conn.getRemoteSocketAddress() + ": " + message);
+            }
+
+            @Override
+            public void onError(WebSocket conn, Exception ex) {
+                System.out.println("Got an error from " + conn.getRemoteSocketAddress() + ": " + ex);
+            }
+
+            @Override
+            public void onStart() {
+                System.out.println("Listening for connections.");
+            }
+        };
+
+        server.start();
     }
 }
