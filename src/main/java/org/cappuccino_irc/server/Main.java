e89f4ff3e847be9a47860cb9bf053f91f992e6aa
Ronald Bunk <me@azoraqua.com>
2021-03-03 12:30:13 +0100

    Updated.
    Signed-off-by: Ronald Bunk <me@azoraqua.com>

--- src/main/java/org/cappuccino_irc/server/Main.java
+++ src/main/java/org/cappuccino_irc/server/Main.java
@@ -5,6 +5,8 @@ import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Main {
 
@@ -12,22 +14,26 @@ public final class Main {
 
     public static void main(String[] args) {
         final WebSocketServer server = new WebSocketServer(new InetSocketAddress(8000)) {
+            final List<WebSocket> sockets = new ArrayList<>();
+
             @Override
-            public void onOpen(WebSocket conn, ClientHandshake handshake) {
-                System.out.println("Got a connection: " + conn.getRemoteSocketAddress());
+            public void onOpen(WebSocket socket, ClientHandshake handshake) {
+                System.out.println("Got a connection: " + socket.getRemoteSocketAddress());
+
+                sockets.add(socket);
             }
 
             @Override
-            public void onClose(WebSocket conn, int code, String reason, boolean remote) { }
+            public void onClose(WebSocket socket, int code, String reason, boolean remote) { }
 
             @Override
-            public void onMessage(WebSocket conn, String message) {
-                System.out.println("Got a message from " + conn.getRemoteSocketAddress() + ": " + message);
+            public void onMessage(WebSocket socket, String message) {
+                System.out.println("Got a message from " + socket.getRemoteSocketAddress() + ": " + message);
             }
 
             @Override
-            public void onError(WebSocket conn, Exception ex) {
-                System.out.println("Got an error from " + conn.getRemoteSocketAddress() + ": " + ex);
+            public void onError(WebSocket socket, Exception ex) {
+                System.out.println("Got an error from " + socket.getRemoteSocketAddress() + ": " + ex);
             }
 
             @Override
