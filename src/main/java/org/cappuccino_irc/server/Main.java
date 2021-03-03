dd93dbdafbab970cc423cf90cbcb503234a8ef92
Ronald Bunk <me@azoraqua.com>
2021-03-03 13:59:53 +0100

    Updated.
    Signed-off-by: Ronald Bunk <me@azoraqua.com>

--- src/main/java/org/cappuccino_irc/server/Main.java
+++ src/main/java/org/cappuccino_irc/server/Main.java
@@ -5,26 +5,23 @@ import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.List;
 
 public final class Main {
 
-    private Main() { }
+    private Main() {
+    }
 
     public static void main(String[] args) {
         final WebSocketServer server = new WebSocketServer(new InetSocketAddress(8000)) {
-            final List<WebSocket> sockets = new ArrayList<>();
 
             @Override
             public void onOpen(WebSocket socket, ClientHandshake handshake) {
                 System.out.println("Got a connection: " + socket.getRemoteSocketAddress());
-
-                sockets.add(socket);
             }
 
             @Override
-            public void onClose(WebSocket socket, int code, String reason, boolean remote) { }
+            public void onClose(WebSocket socket, int code, String reason, boolean remote) {
+            }
 
             @Override
             public void onMessage(WebSocket socket, String message) {
