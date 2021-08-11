cbe10b4c601d959feb0da4cb7437fbcc195c5e91
Ronald Bunk <me@azoraqua.com>
2021-08-12 01:39:50 +0200

    Added support for multi-client.
    Signed-off-by: Ronald Bunk <me@azoraqua.com>

--- src/main/java/org/cappuccino_irc/server/Main.java
+++ src/main/java/org/cappuccino_irc/server/Main.java
@@ -3,74 +3,169 @@ package org.cappuccino_irc.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public final class Main {
 
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+    private static final Map<Integer, Client> CLIENT_MAP = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) throws IOException {
         final ServerSocket server = new ServerSocket(3001);
         System.out.println("Server is listening for connections at port 3001.");
 
-        Socket client;
+        while (!server.isClosed()) {
+            Socket clientSocket;
 
-        while ((client = server.accept()) != null) {
-            final InputStream in = client.getInputStream();
-            final OutputStream out = client.getOutputStream();
-            final Scanner scanner = new Scanner(in, StandardCharsets.UTF_8);
+            while ((clientSocket = server.accept()) != null) {
+                CLIENT_MAP.put(CLIENT_MAP.size() + 1, new Client(clientSocket));
+            }
+        }
+    }
 
-            String data;
+    private static final class Client implements Runnable {
+        private final Socket socket;
+        private final Thread thread;
 
-            while (scanner.hasNext()) {
-                data = scanner.useDelimiter("\r\n\r\n").next();
+        private InputStream in;
+        private OutputStream out;
 
-                System.out.println("Request from " + (client.getInetAddress().getHostAddress() + ":" + client.getPort()) + " (\n" + Arrays.stream(data.split("\r\n")).map(s -> "\t" + s).collect(Collectors.joining("\n")) + "\n)");
+        public Client(Socket socket) {
+            this.socket = socket;
+            this.thread = new Thread(this);
+            this.thread.start();
+        }
 
-                Matcher matcher = Pattern.compile("^GET").matcher(data);
+        public InputStream getInputStream() throws IOException {
+            if (in == null) {
+                in = new EncapsulatedInputStream(socket.getInputStream());
+            }
 
-                if (matcher.find()) {
-                    matcher = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
+            return in;
+        }
 
-                    if (matcher.find()) {
-                        final StringBuilder sb = new StringBuilder();
-                        sb.append("HTTP/1.1 101 Switching Protocols\r\n");
-                        sb.append("Connection: Upgrade\r\n");
-                        sb.append("Upgrade: websocket\r\n");
-                        sb.append("Sec-WebSocket-Accept: ").append(Base64.getEncoder().encodeToString(
-                                MessageDigest.getInstance("SHA-1").digest(
-                                        (matcher.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes(StandardCharsets.UTF_8)
-                                )
-                        ));
-                        sb.append("\r\n\r\n");
+        public OutputStream getOutputStream() throws IOException {
+            if (out == null) {
+                out = new EncapsulatedOutputStream(socket.getOutputStream());
+            }
 
-                        byte[] response = sb.toString().getBytes(StandardCharsets.UTF_8);
-                        out.write(response, 0, response.length);
+            return out;
+        }
 
-//                        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//                        final DataOutputStream daos = new DataOutputStream(baos);
-//
-//                        daos.writeUTF("HTTP/1.1 101 Switching Protocols\r\n");
-//                        daos.writeUTF("Connection: Upgrade\r\n");
-//                        daos.writeUTF("Upgrade: websocket\r\n");
-//                        daos.writeUTF("Sec-WebSocket-Accept: " + Base64.getEncoder().encodeToString(
-//                                        MessageDigest.getInstance("SHA-1").digest(
-//                                                (matcher.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes(StandardCharsets.UTF_8)
-//                                        )
-//                                )
-//                        );
-//                        daos.writeUTF("\r\n\r\n");
-//                        out.write(baos.toByteArray());
+        public void send(byte[] data) throws IOException {
+            if (data == null || data.length == 0) {
+                throw new IllegalArgumentException("parameter 'data' is either null or empty.");
+            }
+
+            socket.getOutputStream().write(data, 0, data.length);
+        }
+
+        @Override
+        public String toString() {
+            return "Client (address=" + socket.getInetAddress().getHostAddress()+ ", port=" + socket.getPort() + ")";
+        }
+
+        @Override
+        public void run() {
+            while (!socket.isClosed()) {
+                try (InputStream in = this.getInputStream()) {
+                    final Scanner scanner = new Scanner(in, StandardCharsets.UTF_8);
+
+                    while (scanner.useDelimiter("\r\n\r\n").hasNext()) {
+                        String data = scanner.useDelimiter("\r\n\r\n").next();
+                        Matcher matcher = Pattern.compile("Connection: Upgrade").matcher(data);
+
+                        if (matcher.find()) {
+                            try {
+                                System.out.println("[INFO] " + this + " protocol upgrade.");
+                                handleProtocolUpgrade(this, data);
+                            } catch (NoSuchAlgorithmException | IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        System.out.println("[INFO] " + this + " {\n" + Arrays.stream(data.split("\r\n")).map(s -> "\t" + s).collect(Collectors.joining("\n")) + "\n}");
                     }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+            }
+        }
+
+        private void handleProtocolUpgrade(Client client, String data) throws NoSuchAlgorithmException, IOException {
+            Matcher matcher = Pattern.compile("^GET").matcher(data);
+
+            if (!matcher.find()) {
+                throw new IOException("Protocol upgrade requires GET method.");
+            }
+
+            if (matcher.find()) {
+                matcher = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
+
+                if (!matcher.find()) {
+                    throw new IOException("Sec-WebSocket-Key is required.");
+                }
+
+                final StringBuilder sb = new StringBuilder();
+                sb.append("HTTP/1.1 101 Switching Protocols\r\n");
+                sb.append("Connection: Upgrade\r\n");
+                sb.append("Upgrade: websocket\r\n");
+                sb.append("Sec-WebSocket-Accept: ").append(Base64.getEncoder().encodeToString(
+                        MessageDigest.getInstance("SHA-1").digest(
+                                (matcher.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes(StandardCharsets.UTF_8)
+                        )
+                ));
+                sb.append("\r\n\r\n");
+
+                byte[] response = sb.toString().getBytes(StandardCharsets.UTF_8);
+                client.send(response);
+            }
+        }
+
+        private static final class EncapsulatedInputStream extends InputStream {
+
+            private final InputStream in;
+
+            public EncapsulatedInputStream(InputStream in) {
+                this.in = in;
+            }
+
+            @Override
+            public int read() throws IOException {
+                return in.read();
+            }
+
+            @Override
+            public void close() {
+                // Prevent closing the stream, because it closes the underlying socket.
+            }
+        }
+
+        private static final class EncapsulatedOutputStream extends OutputStream {
+
+            private final OutputStream out;
+
+            public EncapsulatedOutputStream(OutputStream out) {
+                this.out = out;
+            }
+
+            @Override
+            public void write(int b) throws IOException {
+                out.write(b);
+            }
+
+            @Override
+            public void close() {
+                // Prevent closing the stream, because it closes the underlying socket.
             }
         }
     }
